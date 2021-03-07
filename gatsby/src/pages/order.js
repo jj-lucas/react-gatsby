5d2f06e4bc79bacec51c139a62fbfad56a0f3afa
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-03-07 18:10:16 +0100

    :sparkles: Styling order form
    Scope: Lesson 35

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -4,6 +4,8 @@ import Img from 'gatsby-image'
 import useForm from '../utils/useForm'
 import calculatePizzaPrice from '../utils/calculatePizzaPrice'
 import formatMoney from '../utils/formatMoney'
+import OrderStyles from '../styles/OrderStyles'
+import MenuItemStyles from '../styles/MenuItemStyles'
 
 export default function OrderPage({ data }) {
 	const pizzas = data.pizzas.nodes
@@ -11,49 +13,51 @@ export default function OrderPage({ data }) {
 	const { values, updateValue } = useForm({ name: '', email: '' })
 	return (
 		<>
-			<fieldset>
-				<legend>Your info</legend>
-				<label htmlFor="name">Name</label>
-				<input
-					type="text"
-					name="name"
-					value={values.name}
-					onChange={updateValue}
-				/>
-				<label htmlFor="email">Email</label>
-				<input
-					type="email"
-					name="email"
-					value={values.email}
-					onChange={updateValue}
-				/>
-			</fieldset>
-			<fieldset>
-				<legend>Menu</legend>
-				{pizzas.map(pizza => (
-					<div key={pizza.id}>
-						<Img
-							width="50"
-							height="50"
-							fluid={pizza.image.asset.fluid}
-							alt={pizza.name}
-						/>
-						<div>
-							<h2>{pizza.name}</h2>
-						</div>
-						<div>
-							{['S', 'M', 'L'].map(size => (
-								<button type="button" key={size}>
-									{size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
-								</button>
-							))}
-						</div>
-					</div>
-				))}
-			</fieldset>
-			<fieldset>
-				<legend>Order</legend>
-			</fieldset>
+			<OrderStyles>
+				<fieldset>
+					<legend>Your info</legend>
+					<label htmlFor="name">Name</label>
+					<input
+						type="text"
+						name="name"
+						value={values.name}
+						onChange={updateValue}
+					/>
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						name="email"
+						value={values.email}
+						onChange={updateValue}
+					/>
+				</fieldset>
+				<fieldset className="menu">
+					<legend>Menu</legend>
+					{pizzas.map(pizza => (
+						<MenuItemStyles key={pizza.id}>
+							<Img
+								width="50"
+								height="50"
+								fluid={pizza.image.asset.fluid}
+								alt={pizza.name}
+							/>
+							<div>
+								<h2>{pizza.name}</h2>
+							</div>
+							<div>
+								{['S', 'M', 'L'].map(size => (
+									<button type="button" key={size}>
+										{size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
+									</button>
+								))}
+							</div>
+						</MenuItemStyles>
+					))}
+				</fieldset>
+				<fieldset className="order">
+					<legend>Order</legend>
+				</fieldset>
+			</OrderStyles>
 		</>
 	)
 }
