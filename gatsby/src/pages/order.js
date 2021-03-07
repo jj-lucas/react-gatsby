bca3484f92dc80f60b547953bde1dee1edcf6fd1
Lucas Gabriel Salina D'Ovidio <lucas@lucsali.com>
2021-03-07 18:38:52 +0100

    :sparkles: Custom hook for order form
    Scope: Lesson 36

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -6,11 +6,13 @@ import calculatePizzaPrice from '../utils/calculatePizzaPrice'
 import formatMoney from '../utils/formatMoney'
 import OrderStyles from '../styles/OrderStyles'
 import MenuItemStyles from '../styles/MenuItemStyles'
+import usePizza from '../utils/usePizza'
+import PizzaOrder from '../components/PizzaOrder'
 
 export default function OrderPage({ data }) {
 	const pizzas = data.pizzas.nodes
-
 	const { values, updateValue } = useForm({ name: '', email: '' })
+	const { order, addToOrder, removeFromOrder } = usePizza({ pizzas, values })
 	return (
 		<>
 			<OrderStyles>
@@ -46,7 +48,10 @@ export default function OrderPage({ data }) {
 							</div>
 							<div>
 								{['S', 'M', 'L'].map(size => (
-									<button type="button" key={size}>
+									<button
+										type="button"
+										key={size}
+										onClick={() => addToOrder({ id: pizza.id, size })}>
 										{size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
 									</button>
 								))}
@@ -56,6 +61,11 @@ export default function OrderPage({ data }) {
 				</fieldset>
 				<fieldset className="order">
 					<legend>Order</legend>
+					<PizzaOrder
+						order={order}
+						removeFromOrder={removeFromOrder}
+						pizzas={pizzas}
+					/>
 				</fieldset>
 			</OrderStyles>
 		</>
